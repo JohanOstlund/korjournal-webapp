25ef016e915d21e027c3621f2ce5998a831ad25f
JohanOstlund <ostlund.johan@gmail.com>
2025-09-26 13:25:40 +0200

    Update models.py - user login

--- api/app/models.py
+++ api/app/models.py
@@ -2,6 +2,13 @@ from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, Foreig
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from .db import Base
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(64), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
 
 class Vehicle(Base):
     __tablename__ = "vehicles"
