5076ec1088e190b60d73f46bf0fdd5c72ac52c3a
JohanOstlund <ostlund.johan@gmail.com>
2025-09-27 00:36:34 +0200

    Update models.py - fix

--- api/app/models.py
+++ api/app/models.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, Foreig
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from .db import Base
-from .database import Base
 
 class User(Base):
     __tablename__ = "users"
